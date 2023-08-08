ae33bcafd9ae7a7080e1cd2c8f1a1ed6ff12346f
nguyennamson <namson.nguyen102@gmail.com>
2023-08-08 17:28:24 +0700

    Use simple react router component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import './index.css'
 
+function App() {
+  return (
+    <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function Home() {
+
+  return (
+    <>
+      <h2>Hello React Router</h2>
+    </>
+  )
+}
+
+function About() {
+
+  return (
+    <>
+      <h2>Information about React Router</h2>
+    </>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
