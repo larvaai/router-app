ed2c6e160fe9cb1d1f33cd5c5ed27052c560a68f
nguyennamson <namson.nguyen102@gmail.com>
2023-08-08 23:35:33 +0700

    miragejs server and fetch data rendering Vans page

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
+import Vans from '../pages/Vans'
+
+import "../server"
 
 /**
  * Challenge:
@@ -27,11 +30,13 @@ function App() {
         <Link className="site-logo" to="/">#VANLIFE</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
