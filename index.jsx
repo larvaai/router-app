c9051455809a4c76c20a5d393f2f8b6c5ef3c6da
nguyennamson <namson.nguyen102@gmail.com>
2023-08-15 19:09:16 +0700

    fetching by loader, protected routes

--- index.jsx
+++ index.jsx
@@ -13,45 +13,87 @@ import About from "./pages/About"
 import Dashboard from './pages/Host/Dashboard'
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
+import Login from './pages/Login'
 import Vans, {loader as vansLoader} from "./pages/Vans/Vans"
-import HostVans from "./pages/Host/HostVans.jsx"
-import HostVanDetail from "./pages/Host/HostVanDetail"
+import VanDetail, {loader as vanDetailLoader} from "./pages/Vans/VanDetail"
+import HostVans, {loader as hostVansLoader} from "./pages/Host/HostVans.jsx"
+import HostVanDetail, {loader as hostVanDetailLoader} from "./pages/Host/HostVanDetail"
 import HostVanInfo from "./pages/Host/HostVanInfo"
 import HostVanPricing from "./pages/Host/HostVanPricing"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
-import VanDetail from "./pages/Vans/VanDetail"
 import Layout from './components/Layout'
 import HostLayout from './components/HostLayout'
 import NotFound from './pages/NotFound'
 import Error from './components/Error'
+import { requireAuth } from './utils'
 
 import "./server"
 
 const router = createBrowserRouter(createRoutesFromElements(
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route 
-            path="vans" 
-            element={<Vans />}
-            errorElement={<Error />}
-            loader={vansLoader}
-          />
-          <Route path="vans/:id" element={<VanDetail />} />
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route
+      path="login"
+      element={<Login />}
+    />
+    <Route 
+      path="vans" 
+      element={<Vans />}
+      errorElement={<Error />}
+      loader={vansLoader}
+    />
+    <Route 
+      path="vans/:id" 
+      element={<VanDetail />}
+      loader={vanDetailLoader}
+    />
 
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />}>
-              <Route index element={<HostVanInfo />} />
-              <Route path="pricing" element={<HostVanPricing />} />
-              <Route path="photos" element={<HostVanPhotos />} />
-            </Route>
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Route>
+    <Route path="host" element={<HostLayout />}>
+      <Route 
+        index 
+        element={<Dashboard />}
+        loader={async () => await requireAuth()} 
+      />
+      <Route 
+        path="income" 
+        element={<Income />}
+        loader={async () => await requireAuth()} 
+      />
+      <Route 
+        path="reviews" 
+        element={<Reviews />}
+        loader={async () => await requireAuth()} 
+      />
+      <Route 
+        path="vans" 
+        element={<HostVans />}
+        loader={hostVansLoader} 
+      />
+      <Route 
+        path="vans/:id" 
+        element={<HostVanDetail />}
+        loader={hostVanDetailLoader}
+      >
+        <Route 
+          index 
+          element={<HostVanInfo />}
+          loader={async () => await requireAuth()} 
+        />
+        <Route 
+          path="pricing" 
+          element={<HostVanPricing />}
+          loader={async () => await requireAuth()} 
+        />
+        <Route 
+          path="photos" 
+          element={<HostVanPhotos />}
+          loader={async () => await requireAuth()} 
+        />
+      </Route>
+    </Route>
+    <Route path="*" element={<NotFound />} />
+  </Route>
 ))
 
 function App() {
--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -1,18 +1,22 @@
 import React from "react"
-import { Link, useParams, useLocation } from "react-router-dom"
+import { Link, useParams, useLocation, useLoaderData } from "react-router-dom"
+import { getVans } from '../../api'
+
+export function loader({ params }) {
+  console.log(params)
+  return getVans(params.id)
+}
 
 export default function VanDetail() {
-  const params = useParams();
   const location = useLocation();
-  console.log(location);
+  const van = useLoaderData();
+  console.log(van);
 
-  const [van, setVan] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch(`/api/vans/${params.id}`)
-      .then(res => res.json())
-      .then(data => setVan(data.vans));
-  }, [params.id])
+  // React.useEffect(() => {
+  //   fetch(`/api/vans/${params.id}`)
+  //     .then(res => res.json())
+  //     .then(data => setVan(data.vans));
+  // }, [params.id])
 
   const search = location.state?.search || ""
   const type = location.state?.type || "all"
@@ -25,17 +29,14 @@ export default function VanDetail() {
           className="back-button"
         >&larr; <span>Back to {type} vans</span></Link>
 
-        {van ? (
-          <div className="van-detail">
-            <img src={van.imageUrl} />
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
-            <h2>{van.name}</h2>
-            <p className="van-price"><span>${van.price}</span>/day</p>
-            <p>{van.description}</p>
-            <button className="link-button">Rent this van</button>
-          </div>
-        ) : <h2>Loading...</h2>
-      }
+        <div className="van-detail">
+          <img src={van.imageUrl} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price"><span>${van.price}</span>/day</p>
+          <p>{van.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
       </div>
   )
 };
